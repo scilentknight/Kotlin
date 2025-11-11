ca5da4db0f879154d05d4e1e533294c5e10c41ab
Scilent Knight <scilentknight512@gmail.com>
2025-11-11 17:06:38 +0545

    Variables file added

---
+++ Kotlin/src/Variables.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    // var - var can be reassigned with the same type
+    var a = 40
+    var b = 78.2
+    var c = "Coding Boot"
+    println(a)
+
+    // val -'val' cannot be reassigned.
+    val v = 50
+    println(v)
+
+    // : 'colon' for extends
+    val e : Double = 20.3
+    println(e)
+
+}

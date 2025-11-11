716f26bdbac724ba88cfe5edd4ad3f16ec8a4bd1
Scilent Knight <scilentknight512@gmail.com>
2025-11-11 17:08:20 +0545

    String Interpolation file added

---
+++ Kotlin/src/StringInterpolation.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var a: Int = 10
+    var b: Int = 5
+    println("The value of a is $a")
+    println("The value of b is $b")
+
+    println("The value of $a and $b is: ${a+b}")
+
+}

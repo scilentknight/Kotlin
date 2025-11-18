8235dd026aa45ffa5dc7dad606d3fd563b8b8832
Scilent Knight <scilentknight512@gmail.com>
2025-11-18 22:46:54 +0545

    Constructors file added

---
+++ Kotlin/src/Constructors.kt
@@ -0,0 +1,23 @@
+// Constructor Types
+// 1. Default Constructor / Empty Constructor - var animal = Animal()
+// 2. Primary Constructor
+// 3. Secondary Constructor
+fun main() {
+    var animal = Animal("Cow", 5, "Jersey", "Black")
+    val animal2 = Animal("Dog", "White")
+}
+
+class Animal(var name: String, var age:Int, var breed: String, var color: String){
+    // Secondary constructor
+    constructor(animalName:String, animalColor: String) : this(animalName, 1, "None", animalColor) // here this indicates primary constructor
+
+    init {
+        println("Name of animal is: $name")
+    }
+    init {
+        println("Breed of animal is: $breed")
+    }
+    init {
+        println("Color of animal is: $color")
+    }
+}

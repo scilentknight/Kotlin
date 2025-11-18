d1fe4f8a3a461b04e07b40992e2e40abc498eb14
Scilent Knight <scilentknight512@gmail.com>
2025-11-18 20:13:49 +0545

    ForLoop file added

---
+++ Kotlin/src/ForLoop.kt
@@ -0,0 +1,18 @@
+fun main() {
+//    for(i in 1..10 step 2){ // in keyword
+//        println(i)
+//    }
+
+//    for (i in 1 until 10){ // until keyword
+//        println(i)
+//    }
+
+//    for (i in 10 downTo 1 step 2) // reverse print
+//        println(i)
+
+    // table print
+    var num = 3
+    for(i in 1..10){
+        println("$num*$i = ${num*i}")
+    }
+}

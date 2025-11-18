4fa94b262e9413f3930820c154a8eb9bc1e0f4a3
Scilent Knight <scilentknight512@gmail.com>
2025-11-18 20:01:01 +0545

    ConditionalWhileAndDoWhileStatement file added

---
+++ Kotlin/src/ConditionalWhileAndDoWhileStatement.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var i = 0
+//    while(i<=5){
+//        println("This is Kotlin Course ${i}")
+//        i++
+//    }
+    do {
+        println("This is Kotlin Course ${i}")
+        i++
+    } while (i<=5)
+
+}

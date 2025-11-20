669e2e6e040a85bce651cfa4077b129a90255ec1
Scilent Knight <scilentknight512@gmail.com>
2025-11-20 19:53:20 +0545

    ExtensionFunction file added

---
+++ Kotlin/src/ExtensionFunction.kt
@@ -0,0 +1,22 @@
+fun main() {
+    var circle = Circlee()
+    println(circle.area(5))
+    println(circle.perimeter(4)) // extension function calling
+
+    var str = "Madam"
+    println(str.isPalendrome())
+}
+
+class Circlee {
+    fun area(r: Int): Double {
+        return 3.14 * r * r
+    }
+}
+
+fun Circlee.perimeter(r: Int): Double {
+    return 2 * 3.14 * r
+}
+
+fun String.isPalendrome(): Boolean {
+    return this.lowercase() == this.lowercase().reversed()
+}
